44251bef9fe71954a01187a5263bb3c5b172ecaa
paulinha <paulinha@MacBook-Pro-de-seguranca.local>
2023-06-05 15:51:06 -0300

    Requisito 1

---
+++ src/Races/Race.ts
@@ -0,0 +1,25 @@
+abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+
+  constructor(n: string, d: number) {
+    this._name = n;
+    this._dexterity = d;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;
